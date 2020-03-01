338d90acd29270a9722c4759dbfc8ec3ec9e8734
manvendra09 <ankursingh297@gmail.com>
2020-03-01 12:31:53 +0530

    completed Todo-app

---
+++ myproject/todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from . import views
+
+
+urlpatterns = [
+    
+    path('',views.index,name='index'),
+    path('add',views.addTodo,name='add'),
+    path('complete/<todo_id>',views.completeTodo,name='complete'),
+    path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
+    path('deleteadd', views.deleteAdd, name='deleteadd')
+]
